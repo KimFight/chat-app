3ac8ab667c87f9bba94ad120f579cd75f7edf56a
KimFight <mk_idea_lism@icloud.com>
2020-11-01 15:25:59 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  
   devise_for :users
-  get 'messages/index'
- root to: "messages#index"
-end
+    root to: "messages#index"
+    # root 'コントローラー#アクション'はトップページにアクセスしたときのルーティングを
+    # MessagesController の index アクションに設定するという意。
+   
+    resources :users, only: [:edit, :update]
+    #resources :●●と書くのみで複数のルーティングが定義される。
+  end
