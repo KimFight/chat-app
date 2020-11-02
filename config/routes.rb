2a4c8d1904961eedd5d54a4c9a5b0b14c6092c56
KimFight <mk_idea_lism@icloud.com>
2020-11-02 20:16:22 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   
   devise_for :users
-    root  to: "rooms#index"
+    root to: "rooms#index"
     # root to: 'コントローラー#アクション'はトップページにアクセスしたときのルーティング
     
     resources :users, only: [:edit, :update]
     #resources :●●と書くのみで複数のルーティングが定義される。
-    resources :rooms, only: [:new, :create]
+    resources :rooms, only: [:new, :create] do
+      resources :messages, only: [:index, :create]
+    end
+    # do ~ end でネスト。
+    # roomsが親で、messagesが子という親子関係(チャットルームに属しているメッセージという意味)
   end
