bf702fbafafe41da9ae8fb3559b079b8aef86e18
KimFight <mk_idea_lism@icloud.com>
2020-11-03 12:43:28 +0900

    Active Storageの導入

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,10 @@ class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
   # userテーブルとroomテーブルにも記入を忘れない
+  has_one_attached :image
+  # 一対一の関係。記述したモデルの各レコードは、それぞれ1つのファイルを添付できる。
+  # message.image で添付ファイルにアクセスできるようになった。
+
 
   validates :content, presence: true
   # validates はモデルに記述。
